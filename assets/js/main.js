1f9d59e1d7761ca6926860803e5a2da3bf9739c3
Kateryna Navoienko <enavoyenko@gmail.com>
2022-08-15 22:21:10 +0300

    menu hamburger

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   $('.header-navbar__btn').click(function () {
-    $(this).toggleClass('header-navbar__btn--active');
+    $(this).toggleClass("header-navbar__btn--active");
   });
 });
